f85413fec8fc869ece93f527c15d3ae5257958ee
Valneja <vs7315@student.uni-lj.si>
2016-03-15 09:44:54 +0000

    Kočnane vaje

--- FriBox.js
+++ FriBox.js
@@ -16,6 +16,8 @@ var streznik = http.createServer(function(zahteva, odgovor) {
        posredujOsnovnoStran(odgovor);
    } else if (zahteva.url == '/datoteke') { 
        posredujSeznamDatotek(odgovor);
+   } else if (zahteva.url.startsWith('/poglej')){
+       posredujStaticnoVsebino(odgovor, dataDir + zahteva.url.replace("/poglej", ""), "");
    } else if (zahteva.url.startsWith('/brisi')) { 
        izbrisiDatoteko(odgovor, dataDir + zahteva.url.replace("/brisi", ""));
    } else if (zahteva.url.startsWith('/prenesi')) { 
@@ -31,18 +33,32 @@ function posredujOsnovnoStran(odgovor) {
     posredujStaticnoVsebino(odgovor, './public/fribox.html', "");
 }
 
+function izbrisiDatoteko(odgovor, datoteka){
+    odgovor.writeHead(200, {'Content-Type': 'text/plain'});
+    fs.unlink(datoteka, function(napaka){
+        if(napaka){
+            posredujNapako404(odgovor);
+        } else{
+            odgovor.write("Datoteka izbrisana!");
+            odgovor.end();
+        }
+    });
+}
+
 function posredujStaticnoVsebino(odgovor, absolutnaPotDoDatoteke, mimeType) {
         fs.exists(absolutnaPotDoDatoteke, function(datotekaObstaja) {
             if (datotekaObstaja) {
                 fs.readFile(absolutnaPotDoDatoteke, function(napaka, datotekaVsebina) {
                     if (napaka) {
                         //Posreduj napako
+                        posredujNapako404(odgovor);
                     } else {
                         posredujDatoteko(odgovor, absolutnaPotDoDatoteke, datotekaVsebina, mimeType);
                     }
                 })
             } else {
                 //Posreduj napako
+                posredujNapako404(odgovor);
             }
         })
 }
@@ -62,6 +78,7 @@ function posredujSeznamDatotek(odgovor) {
     fs.readdir(dataDir, function(napaka, datoteke) {
         if (napaka) {
             //Posreduj napako
+            posredujNapako500(odgovor);
         } else {
             var rezultat = [];
             for (var i=0; i<datoteke.length; i++) {
@@ -89,9 +106,27 @@ function naloziDatoteko(zahteva, odgovor) {
         fs.copy(zacasnaPot, dataDir + datoteka, function(napaka) {  
             if (napaka) {
                 //Posreduj napako
+                
             } else {
                 posredujOsnovnoStran(odgovor);        
             }
         });
     });
+}
+
+streznik.listen(process.env.PORT, function() {
+    console.log("Strežnik je pognan!");
+})
+
+// NAPAKE
+function posredujNapako404(odgovor){
+    odgovor.writeHead(404, {'Content-Type': 'text/plain'});
+    odgovor.write('Napaka 404: Vira ni mogoče najti!');
+    odgovor.end();
+}
+
+function posredujNapako500(odgovor){
+    odgovor.writeHead(500, {'Content-Type': 'text/plain'});
+    odgovor.write('Napaka 500: Prišlo je do napake strežnika!');
+    odgovor.end();
 }
